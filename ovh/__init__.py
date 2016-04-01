60b5a2cb257d4862fcb8f9d94781be47d218c6a9
Jean-Tiare Le Bigot <jean-tiare.le-bigot@corp.ovh.com>
2016-04-01 14:34:30 +0200

    update copyright headers
    Signed-off-by: Jean-Tiare Le Bigot <jean-tiare.le-bigot@corp.ovh.com>

--- ovh/__init__.py
+++ ovh/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 #
-# Copyright (c) 2013-2014, OVH SAS.
+# Copyright (c) 2013-2016, OVH SAS.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
